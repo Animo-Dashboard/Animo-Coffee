687d50fa31d701114f10cbe49d856d579f72af81
Alex <alex.van.der.kroft@student.nhlstenden.com>
2023-05-11 13:38:59 +0200

    Removed unneccesary UUID

--- new_app2/lib/main.dart
+++ new_app2/lib/main.dart
@@ -7,7 +7,6 @@ import 'dart:io' show Platform;
 
 import 'package:permission_handler/permission_handler.dart';
 
-Uuid _UART_UUID = Uuid.parse("6E400001-B5A3-F393-E0A9-E50E24DCCA9E");
 void main() {
   runApp(const MyApp());
 }
