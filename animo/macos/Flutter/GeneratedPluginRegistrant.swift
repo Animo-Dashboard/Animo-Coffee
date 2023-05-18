d4aa71408be7ebb280818351e68fa33f99b1c735
Alex <alex.van.der.kroft@student.nhlstenden.com>
2023-05-18 15:41:37 +0200

    Fixed log-in, registration and validation

--- animo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ animo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import flutter_blue
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
