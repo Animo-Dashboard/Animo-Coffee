dcebc7a9d443f1fdf6db53df0e06c0a7e3260b8e
HolyMelonSeed <sikora2012alex@gmail.com>
2023-05-24 03:27:32 +0200

    Finish styling and data for statistics page

--- animo/lib/pages/deviceStatistics_page.dart
+++ animo/lib/pages/deviceStatistics_page.dart
@@ -18,6 +18,10 @@ class _DeviceStatisticsPage extends State<DeviceStatisticsPage> {
   String pageTitle = "Page";
   Device device = Device('name', 'model', "serialNumber", "", Timestamp.now(),
       Timestamp.now(), 0, 0, 0, 0, 0, 0);
+  EdgeInsets headerPadding =
+      const EdgeInsets.only(top: 8, bottom: 8, left: 16, right: 16);
+  EdgeInsets nonHeaderPadding =
+      const EdgeInsets.only(left: 28, right: 16, top: 5, bottom: 8);
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +29,7 @@ class _DeviceStatisticsPage extends State<DeviceStatisticsPage> {
         <String, dynamic>{}) as Map;
     try {
       var data = arguments["device"];
+      pageTitle = data["Name"];
       device = Device(
         data["Name"] as String? ?? "Default Name",
         data["Model"] as String? ?? "Default Model",
@@ -47,72 +52,304 @@ class _DeviceStatisticsPage extends State<DeviceStatisticsPage> {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         body: Container(
-            decoration: getAppBackground(),
-            child: Column(
-              children: [
-                Container(
-                    decoration: getBackgroundIfError(),
-                    child: Padding(
+          height: MediaQuery.of(context).size.height,
+          decoration: getAppBackground(),
+          child: SingleChildScrollView(
+              child: Column(
+            children: [
+              Container(
+                  decoration: getBackgroundIfError(),
+                  child: Padding(
                       padding: const EdgeInsets.only(
                           top: 12, bottom: 12, left: 25, right: 25),
-                      child: Row(
+                      child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              right: 12,
-                            ),
-                            child: SizedBox(
-                                height: 170,
-                                child: Image.asset(
-                                  "images/${device.model}.png",
-                                  errorBuilder: (BuildContext context,
-                                      Object exception,
-                                      StackTrace? stackTrace) {
-                                    return Image.asset(
-                                        "images/touch2.png"); // Fallback image
-                                  },
-                                )),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                  right: 12,
+                                ),
+                                child: SizedBox(
+                                    height: 170,
+                                    child: Image.asset(
+                                      "images/${device.model}.png",
+                                      errorBuilder: (BuildContext context,
+                                          Object exception,
+                                          StackTrace? stackTrace) {
+                                        return Image.asset(
+                                            "images/touch2.png"); // Fallback image
+                                      },
+                                    )),
+                              ),
+                              Expanded(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    SizedBox(
+                                      height: 40,
+                                      child: TextFormField(
+                                        initialValue: device.name,
+                                        decoration: const InputDecoration(
+                                            contentPadding: EdgeInsets.zero,
+                                            suffixIcon: Icon(
+                                              Icons.edit,
+                                            ),
+                                            suffixIconConstraints:
+                                                BoxConstraints(
+                                                    maxWidth: 24,
+                                                    maxHeight: 24)),
+                                        style: const TextStyle(
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 22),
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 6,
+                                    ),
+                                    Text(device.model),
+                                    Text(
+                                      device.serialNumber,
+                                      style:
+                                          TextStyle(color: CustomColors.grey),
+                                    ),
+                                    const SizedBox(
+                                      height: 6,
+                                    ),
+                                    const Text("Installation date"),
+                                    Text(DateTime.fromMillisecondsSinceEpoch(
+                                            device.installationDate
+                                                .millisecondsSinceEpoch)
+                                        .toString()),
+                                    const SizedBox(
+                                      height: 6,
+                                    ),
+                                    const Text("Last accessed"),
+                                    Text(DateTime.fromMillisecondsSinceEpoch(
+                                            device.lastAccessDate
+                                                .millisecondsSinceEpoch)
+                                        .toString()),
+                                    TextButton(
+                                        onPressed: () {
+                                          //TODO: delete device
+                                        },
+                                        style: const ButtonStyle(
+                                            padding: MaterialStatePropertyAll(
+                                                EdgeInsets.zero)),
+                                        child: Text(
+                                          "Delete device",
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: CustomColors.red,
+                                              fontWeight: FontWeight.w700),
+                                        )),
+                                  ],
+                                ),
+                              )
+                            ],
                           ),
-                          Expanded(
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
+                          Text(
+                            device.error,
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: CustomColors.red,
+                                fontWeight: FontWeight.w500),
+                          ),
+                        ],
+                      ))),
+              Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(top: 16, bottom: 16),
+                    child: Text(
+                      "Device statistics",
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 22),
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                          color: Colors.black38,
+                          child: Padding(
+                            padding: headerPadding,
+                            child: Row(
                               children: [
                                 SizedBox(
-                                  height: 40,
-                                  child: TextFormField(
-                                    initialValue: device.name,
-                                    decoration: const InputDecoration(
-                                        contentPadding: EdgeInsets.zero,
-                                        suffixIcon: Icon(
-                                          Icons.edit,
-                                        ),
-                                        suffixIconConstraints: BoxConstraints(
-                                            maxWidth: 24, maxHeight: 24)),
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 22),
+                                  width:
+                                      (MediaQuery.of(context).size.width - 32) /
+                                          3 *
+                                          2,
+                                  child: const Text(
+                                    "Total drinks made",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w500),
                                   ),
-                                )
+                                ),
+                                Text(
+                                    '${device.coffeeBrewed + device.hotChocolateBrewed + device.teaBrewed}',
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.w500)),
                               ],
                             ),
-                          )
-                        ],
-                      ),
-                    )),
-                Expanded(
-                    child: Column(
-                  children: [
-                    const Text("Statistics"),
-                    Container(
-                      child: Column(
-                        children: [],
-                      ),
-                    )
-                  ],
-                )),
-              ],
-            )),
+                          )),
+                      Container(
+                          color: Colors.black12,
+                          child: Padding(
+                            padding: nonHeaderPadding,
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Coffee",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.coffeeBrewed}',
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Tea",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.teaBrewed}',
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Hot chocolate",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.hotChocolateBrewed}',
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                          color: Colors.black38,
+                          child: Padding(
+                            padding: headerPadding,
+                            child: Row(
+                              children: [
+                                SizedBox(
+                                  width:
+                                      (MediaQuery.of(context).size.width - 32) /
+                                          3 *
+                                          2,
+                                  child: const Text(
+                                    "Ingredients",
+                                    style:
+                                        TextStyle(fontWeight: FontWeight.w500),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      Container(
+                          color: Colors.black12,
+                          child: Padding(
+                            padding: nonHeaderPadding,
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Beans",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.beansPerc}%',
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Milk",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.milkPerc}%',
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width:
+                                          (MediaQuery.of(context).size.width -
+                                                  44) /
+                                              3 *
+                                              2,
+                                      child: const Text(
+                                        "Chocolate",
+                                      ),
+                                    ),
+                                    Text(
+                                      '${device.chocolatePerc}%',
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )),
+        ),
         appBar: getAppBar(
           context,
           pageTitle,
