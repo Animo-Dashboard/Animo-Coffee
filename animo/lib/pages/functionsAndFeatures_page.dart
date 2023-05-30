77c920c5e12a0707431c1aa0a8c6d1d5943e9e40
Robin <57666995+PresentRobin@users.noreply.github.com>
2023-05-30 12:39:21 +0200

    Update functionsAndFeatures_page.dart
    Updated the functions and features page so it shows a dropdown where the user can choose from installed machines or have the option to add a new name (in case they want to see the specs of a machine that isn't installed for some reason)

--- animo/lib/pages/functionsAndFeatures_page.dart
+++ animo/lib/pages/functionsAndFeatures_page.dart
@@ -3,24 +3,33 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
-class MachineSpecsPage extends StatefulWidget {
+class functionsAndFeaturesPage extends StatefulWidget {
   @override
-  _MachineSpecsPageState createState() => _MachineSpecsPageState();
+  _functionsAndFeaturesPageState createState() =>
+      _functionsAndFeaturesPageState();
 }
 
-class _MachineSpecsPageState extends State<MachineSpecsPage> {
-  DatabaseReference _databaseReference =
+class _functionsAndFeaturesPageState extends State<functionsAndFeaturesPage> {
+  DatabaseReference _databaseReferenceMachineSpecs =
       FirebaseDatabase.instance.ref().child("MachineSpecs");
+  DatabaseReference _databaseReferenceMachines =
+      FirebaseDatabase.instance.ref().child("Machines");
   Map<String, dynamic> _machineSpecs = {};
+  List<String> _machineModels = [];
+  String _selectedModel = '';
+  String _searchedName = '';
+  TextEditingController _manualSearchController = TextEditingController();
+  bool _showNotFoundMessage = false;
 
   @override
   void initState() {
     super.initState();
     _fetchMachineSpecs();
+    _fetchMachineModels();
   }
 
   void _fetchMachineSpecs() {
-    _databaseReference.once().then((DataSnapshot snapshot) {
+    _databaseReferenceMachineSpecs.once().then((DataSnapshot snapshot) {
           if (snapshot.value != null) {
             setState(() {
               _machineSpecs = Map<String, dynamic>.from(
@@ -28,6 +37,55 @@ class _MachineSpecsPageState extends State<MachineSpecsPage> {
             });
           }
         } as FutureOr Function(DatabaseEvent value));
+  }
+
+  void _fetchMachineModels() {
+    _databaseReferenceMachines.once().then((DataSnapshot snapshot) {
+          if (snapshot.value != null) {
+            setState(() {
+              List<dynamic> machineModelsList = snapshot.value as List<dynamic>;
+              _machineModels =
+                  machineModelsList.map((model) => model.toString()).toList();
+              _machineModels.add("Can't find the machine you're looking for?");
+            });
+          }
+        } as FutureOr Function(DatabaseEvent value));
+  }
+
+  void _searchMachine() {
+    setState(() {
+      _selectedModel = '';
+      _searchedName = '';
+      _showNotFoundMessage = false;
+    });
+
+    if (_selectedModel.isNotEmpty) {
+      if (_selectedModel == "Can't find the machine you're looking for?") {
+        String manualSearchName = _manualSearchController.text;
+        _searchByName(manualSearchName);
+      } else {
+        _searchByName(_selectedModel);
+      }
+    }
+  }
+
+  void _searchByName(String name) {
+    bool found = false;
+    _machineSpecs.forEach((_, value) {
+      if (value['Name'] == name) {
+        setState(() {
+          _searchedName = name;
+        });
+        found = true;
+        return;
+      }
+    });
+
+    if (!found) {
+      setState(() {
+        _showNotFoundMessage = true;
+      });
+    }
   }
 
   Widget _buildAccordion(String columnName, dynamic columnValue) {
@@ -42,16 +100,89 @@ class _MachineSpecsPageState extends State<MachineSpecsPage> {
     );
   }
 
+  List<DropdownMenuItem<String>> _buildDropdownItems() {
+    return _machineModels.map((String model) {
+      return DropdownMenuItem<String>(
+        value: model,
+        child: Text(model),
+      );
+    }).toList();
+  }
+
+  Widget _buildManualSearchField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: TextField(
+        controller: _manualSearchController,
+        decoration: InputDecoration(
+          labelText: 'Enter machine name',
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _buildMachineSpecs() {
+    if (_searchedName.isEmpty) {
+      return [];
+    } else {
+      List<Widget> accordions = [];
+      _machineSpecs.forEach((key, value) {
+        if (value['Name'] == _searchedName) {
+          value.forEach((columnName, columnValue) {
+            accordions.add(_buildAccordion(columnName, columnValue));
+          });
+        }
+      });
+      return accordions;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Machine Specs'),
       ),
-      body: ListView(
-        children: _machineSpecs.entries.map((entry) {
-          return _buildAccordion(entry.key, entry.value);
-        }).toList(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: DropdownButtonFormField<String>(
+              value: _selectedModel,
+              items: _buildDropdownItems(),
+              onChanged: (String? newValue) {
+                setState(() {
+                  _selectedModel = newValue!;
+                });
+              },
+              decoration: InputDecoration(
+                labelText: 'Select machine model',
+              ),
+            ),
+          ),
+          if (_selectedModel == "Can't find the machine you're looking for?")
+            _buildManualSearchField(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: ElevatedButton(
+              onPressed: _searchMachine,
+              child: Text('Search'),
+            ),
+          ),
+          if (_showNotFoundMessage)
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                "Can't find a machine with the name: $_selectedModel",
+                style: TextStyle(fontSize: 16.0),
+              ),
+            ),
+          Expanded(
+            child: ListView(
+              children: _buildMachineSpecs(),
+            ),
+          ),
+        ],
       ),
     );
   }
