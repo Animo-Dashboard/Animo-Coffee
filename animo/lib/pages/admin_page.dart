297ec43f23013a38e7847c6f8bcc05ec549e8b4a
Robin <57666995+PresentRobin@users.noreply.github.com>
2023-06-06 20:18:09 +0200

    Added all maintenance pages, selection page and images
    this singlehandedly gave me carpal tunnel

--- animo/lib/pages/admin_page.dart
+++ animo/lib/pages/admin_page.dart
@@ -1,3 +1,4 @@
+import 'package:animo/pages/MaintenanceSelection_page.dart';
 import 'package:flutter/material.dart';
 import 'package:animo/inAppFunctions.dart';
 import 'package:animo/reuseWidgets.dart';
@@ -57,10 +58,16 @@ class _AdminPageState extends State<AdminPage> {
                 height: 20,
               ),
               ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    'LOREM IPSUM',
-                  )),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => MaintenanceSelectionPage(),
+                    ),
+                  );
+                },
+                child: Text('Go to Maintenance Selection'),
+              ),
             ],
           )),
     );
