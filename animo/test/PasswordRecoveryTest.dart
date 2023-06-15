2121491b1154b03fefc8dadc9487a0999bba8c9d
Alex <alex.van.der.kroft@student.nhlstenden.com>
2023-06-15 17:26:24 +0200

    Tested Registration and started on dashboard testing

--- animo/test/PasswordRecoveryTest.dart
+++ animo/test/PasswordRecoveryTest.dart
@@ -17,37 +17,8 @@ class MockCollectionReference extends Mock implements CollectionReference {}
 // Mock DocumentReference
 class MockDocumentReference extends Mock implements DocumentReference {}
 
-// Mock FirebaseService
-class MockFirebaseService extends Mock implements FirebaseService {}
-
-class FirebaseService {
-  final FirebaseAuth firebaseAuth;
-  final FirebaseFirestore firestore;
-
-  FirebaseService({required this.firebaseAuth, required this.firestore});
-
-  Future<UserCredential> createUserWithEmailAndPassword(
-      {required String email, required String password}) async {
-    return await firebaseAuth.createUserWithEmailAndPassword(
-      email: email,
-      password: password,
-    );
-  }
-
-  Future<void> sendVerificationEmail(String userEmail) async {
-    // Implement the sendVerificationEmail logic here
-  }
-
-  Future<void> saveUserToFirestore(
-      String uid, String email, String password) async {
-    final userRef = firestore.collection('Users').doc(uid);
-    await userRef.set({
-      'email': email,
-      'password': password,
-      'verified': false,
-    });
-  }
-}
+// Mock UserCredential
+class MockUserCredential extends Mock implements UserCredential {}
 
 void main() {
   late RegistrationPage registrationPage;
@@ -55,7 +26,6 @@ void main() {
   late MockFirebaseFirestore mockFirebaseFirestore;
   late MockCollectionReference mockCollectionReference;
   late MockDocumentReference mockDocumentReference;
-  late MockFirebaseService mockFirebaseService;
 
   setUp(() {
     registrationPage = RegistrationPage();
@@ -65,27 +35,28 @@ void main() {
     mockFirebaseFirestore = MockFirebaseFirestore();
     mockCollectionReference = MockCollectionReference();
     mockDocumentReference = MockDocumentReference();
-    mockFirebaseService = MockFirebaseService();
 
     // Provide mock dependencies
-    when(mockFirebaseService.createUserWithEmailAndPassword(
-            email: anyNamed('email'), password: anyNamed('password')))
-        .thenAnswer((_) async => UserCredentialMock());
-
-    when(mockFirebaseService.sendVerificationEmail(any))
-        .thenAnswer((_) async {});
-
-    when(mockFirebaseService.firestore).thenReturn(mockFirebaseFirestore);
-
-    registrationPage.firebaseService = mockFirebaseService;
+    registrationPage.firebaseAuth = mockFirebaseAuth;
+    registrationPage.firestore = mockFirebaseFirestore;
 
     // Mock Firestore behavior
-    when(mockFirebaseFirestore.collection(any))
-        .thenReturn(mockCollectionReference);
+    when(mockFirebaseFirestore.collection(any)).thenReturn(
+        mockCollectionReference as CollectionReference<Map<String, dynamic>>);
     when(mockCollectionReference.doc(any)).thenReturn(mockDocumentReference);
   });
 
   testWidgets('RegistrationPage form submission', (WidgetTester tester) async {
+    // Mock FirebaseService methods
+    when(mockFirebaseAuth.createUserWithEmailAndPassword(
+      email: anyNamed('email'),
+      password: anyNamed('password'),
+    )).thenAnswer((_) async => MockUserCredential());
+
+    when(mockFirebaseAuth.currentUser).thenReturn(MockUser());
+
+    when(mockDocumentReference.set(any)).thenAnswer((_) async => null);
+
     await tester.pumpWidget(MaterialApp(home: registrationPage));
 
     // Fill in the form
@@ -97,10 +68,10 @@ void main() {
 
     // Simulate form submission
     await tester.tap(find.byType(ElevatedButton));
-    await tester.pump();
+    await tester.pumpAndSettle();
 
-    // Verify that the FirebaseService createUserWithEmailAndPassword method was called
-    verify(mockFirebaseService.createUserWithEmailAndPassword(
+    // Verify that the FirebaseAuth createUserWithEmailAndPassword method was called
+    verify(mockFirebaseAuth.createUserWithEmailAndPassword(
       email: 'test@example.com',
       password: 'password',
     )).called(1);
@@ -111,10 +82,6 @@ void main() {
       'password': 'password',
       'verified': false,
     })).called(1);
-
-    // Verify that the FirebaseService sendVerificationEmail method was called
-    verify(mockFirebaseService.sendVerificationEmail('test@example.com'))
-        .called(1);
 
     // You can add additional assertions as needed to validate the behavior of the registration process
   });
